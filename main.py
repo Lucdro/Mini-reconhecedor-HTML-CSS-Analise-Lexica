e6fcd4f8f0ed7df1e1922b306c127b750029b5a1
Lamberts2002 <gabriellambertsantos@gmail.com>
2023-09-26 18:50:51 -0300

    Add_modify_def's

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def GetTag(input,index):
     else:
         return '' 
 
+###### DEVE SER RESOLVIDO UM BUG AQUI NESSA DEF QUE ESTÁ REPETINDO O "CONTEUDO"####
 def GetContent(input,index):
     inputLen = len(input)
     buffer = ''
@@ -47,8 +48,19 @@ def IsSingleTag(tag):
             return True 
     return False
 
-def GetAtributes(tag):
-    return 
+def GetAttributes(tag):
+    attributes = re.findall(r'(\S+)\s*=\s*"([^"]*)"', tag)
+    if attributes:
+        return attributes
+    else:
+        return []
+
+def GetAttributeNames(attributes):
+    return [attribute[0] for attribute in attributes]
+
+def GetAttributeNames(attributes):
+    return [attribute[1] for attribute in attributes]
+
 
 input = '<html><head> <title> Compiladores </title> </head><body> <p style="color:red;background:blue;" id="abc"> Unipinhal </p> <br> </body></html>'
 
@@ -73,27 +85,24 @@ while True:
         if GetNextChar(input,index) == '/':
             tag = GetTag(input,index)
             index += len(tag) -1
-            tagStack.pop()
-            print(f"Tag de fechamento: {tag}, Nível {len(tagStack)}")
-        #If is opening caracter
+            if len(tagStack) > 0:
+                print(f"Tag de fechamento: {tag}, Nível {len(tagStack)}")
+            else:
+                print(f"Tag de fechamento: {tag}, Nível 0")
+            if len(tagStack) > 0 and tagStack[-1] == tag:
+                tagStack.pop()
         else: 
             tag = GetTag(input,index)
             index += len(tag) - 1 
             print(f"Tag de abertura: {tag}, Nível {len(tagStack)}")
             if not IsSingleTag(tag):
                 tagStack.append(tag)
-            while True:
-                atributes = GetAtributes(tag)
-                if len(atributes) > 0:
-                    for atrib in atributes:
-                        atribName = GetAtributeName(atrib)
-                        value = GetAtributeValue(atrib)
-                        print(f"Atributo: {atribName} Valor: {value}") 
-                        break
-                    else:
-                        break
-    #If is just a caracter
+            attributes = GetAttributes(tag)
+            attribute_names = GetAttributeNames(attributes)
+            attribute_values = GetAttributeValues(attributes)
+            for i in range(len(attribute_names)):
+                print(f"Atributo: {attribute_names[i]} Valor: {attribute_values[i]}")
     elif char != ' ':
-        content = GetContent(input,index)
-        index += len(content) -1 
-        print(f"Conteúdo da tag: {content}")
+        content = GetContent(input, index)
+        if content.strip() != '':
+            print(f"Conteúdo: {content.strip()}, Nível {len(tagStack)}")
